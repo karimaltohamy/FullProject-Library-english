4f44f6de155d0f65f585bcbdbc2cd783d34ad8f6
karimaltohamy <Karimbadr2003@gmail.com>
2022-11-29 11:23:27 +0200

    update

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -13,3 +13,21 @@ close.addEventListener("click", () => {
 });
 
 // toggle lang
+
+let itemOpenLang = document.querySelector(".item-open-lang");
+let menuLang = document.querySelector(".menu-lang");
+
+itemOpenLang.addEventListener("click", () => {
+  menuLang.classList.toggle("open-lang");
+});
+
+// toggle web list
+
+let iconListWeb = document.querySelector(".icon-list-web");
+let aside = document.querySelector(".aside");
+let content = document.querySelector(".content");
+
+iconListWeb.addEventListener("click", () => {
+  aside.classList.toggle("active-aside");
+  content.classList.toggle("active-margin");
+});
