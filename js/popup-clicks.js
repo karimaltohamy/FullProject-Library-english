8d638d510f501456aea4ed0bc5e9e6aa4dbbb94e
karimaltohamy <Karimbadr2003@gmail.com>
2022-12-02 17:05:12 +0200

    book details update

--- js/popup-clicks.js
+++ js/popup-clicks.js
@@ -48,8 +48,13 @@ closeDownloadLink.addEventListener("click", () => {
 let evaluationBtn = document.querySelector(".evaluation");
 let evaluationBox = document.querySelector(".evaluation-box");
 let closeEvaluation = document.querySelector(".close-evaluation");
+let btnBoxClickApp = document.querySelector(".rate-this-book");
 
 evaluationBtn.addEventListener("click", () => {
+  evaluationBox.classList.add("active-evaluation-box");
+});
+
+btnBoxClickApp.addEventListener("click", () => {
   evaluationBox.classList.add("active-evaluation-box");
 });
 
